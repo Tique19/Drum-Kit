fe265aa8b63acc6a2106120893b34d6ba46f306b
Tyler King <Tyler.19.king@gmail.com>
2022-03-09 15:09:53 -0500

    added toms to all buttons

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 for(var i=0; i<document.querySelectorAll(".drum").length; i++){
-  document.querySelectorAll(".drum")[i].addEventListener("click",  handleClick);
+  document.querySelectorAll(".drum")[i].addEventListener("click",  function(){
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+  });
 }
 function handleClick(){
   alert("I got clicked!");
